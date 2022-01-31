9013c7251bfc979874f6cabcfc51dc283593be69
Sha Md. Monem Uddin <67251114+Xxdeadline@users.noreply.github.com>
2022-01-31 17:16:13 +0600

    Add files via upload

---
+++ Task1.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+   <h1>Registration-Form</h1> 
+</head>
+<body>
+    <hr>
+<form>
+    <table>
+    <tr>
+       First Name : 
+       <input type="text" name="First Name" ><br>
+  
+       Last Name :      
+        <input type="text" name="Last Name" ><br>
+        Age :
+        <input type="number" name="Age" ><br>
+
+        Designation :
+        <input type="radio" name="designation" >Junior Programmer 
+        <input type="radio" name="designation" >Senior Programmer 
+        <input type="radio" name="designation" >Project Lead<br>
+
+        Preferred Language :
+        <input type="checkbox" name="Language" >Java 
+        <input type="checkbox" name="Language" >PHP 
+        <input type="checkbox" name="Language" >C++ <br>
+
+        E-mail :
+        <input type="text" name="e-mail" > <br>
+
+        Password :
+        <input type="password" name="pasword" > <br>
+
+        Please Choose file :
+        <input type="submit" name="submit" value="Choose File"> No File Chosen<br>
+        
+        <input type="submit" name="submit" value="Submit"> 
+        
+        <input type="reset" name="reset" value="Reset"> 
+</form>
+</body>
+</html>
